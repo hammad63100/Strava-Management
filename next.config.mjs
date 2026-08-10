699ee8dc3e2313092f91f75e9b798639086d0a21
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2026-08-10 12:42:30 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,6 @@ const nextConfig = {
     unoptimized: true,
   },
   // If you notice broken styles or images on GitHub pages, you may need to uncomment and use the lines below:
-  // basePath: '/Strava-Management',
+  basePath: '/Strava-Management',
 };
 export default nextConfig;
